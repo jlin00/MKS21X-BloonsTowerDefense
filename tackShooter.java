d159accfab05f8e158043c919efcbea06449c4a9
TiffanyCao <tcao00@stuy.edu>
2019-01-14 10:14:46 -0500

    updated attack method

--- tackShooter.java
+++ tackShooter.java
@@ -1,6 +1,9 @@
+import java.util.*;
+
 public class tackShooter extends Tower{
 
   private List<Tack> tacks = new ArrayList<Tack>();
+
   /**A Tower constructor
   *@param int xCord is the x position of the tower, also its row in the array
   *@param int yCord is the y position of the tower, also its column in the array
@@ -14,6 +17,9 @@ public class tackShooter extends Tower{
     radius = rad;
   }
 
+  /**A method that creates four tacks and calls for them to move and attack
+  *once the tack reaches outside of the radius, it is removed from the list of tacks
+  */
   public void attack(){
     tacks.add(new Tack(this.getX(), this.getY(), 1));
     tacks.add(new Tack(this.getX(), this.getY(), 2));
@@ -21,6 +27,9 @@ public class tackShooter extends Tower{
     tacks.add(new Tack(this.getX(), this.getY(), 4));
     for(int i = 0; i < tacks.size(); i++){
       Tack temp = tacks.get(i);
+      if(temp.getSteps() > this.getRadii()){
+        tacks.remove(i);
+      }
       if(temp.getSteps() < this.getRadii()){
         temp.move();
       }
