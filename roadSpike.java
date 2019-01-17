5c5d5f0011e5ec3cb59770c3c769760d9024def7
Jackie Lin <jlin00@stuy.edu>
2019-01-17 09:01:23 -0500

    updated imports

--- roadSpike.java
+++ roadSpike.java
@@ -1,20 +1,34 @@
-public class roadSpike extends Tower{
+//API : http://mabe02.github.io/lanterna/apidocs/2.1/
+import com.googlecode.lanterna.terminal.Terminal.SGR;
+import com.googlecode.lanterna.TerminalFacade;
+import com.googlecode.lanterna.input.Key;
+import com.googlecode.lanterna.input.Key.Kind;
+import com.googlecode.lanterna.terminal.Terminal;
+import com.googlecode.lanterna.terminal.Terminal.Color;
+import com.googlecode.lanterna.terminal.TerminalSize;
+import com.googlecode.lanterna.LanternaException;
+import com.googlecode.lanterna.input.CharacterPattern;
+import com.googlecode.lanterna.input.InputDecoder;
+import com.googlecode.lanterna.input.InputProvider;
+import com.googlecode.lanterna.input.Key;
+import com.googlecode.lanterna.input.KeyMappingProfile;
+import com.googlecode.lanterna.screen.*;
+import java.util.*;
+
+public class RoadSpike extends Tower{
 
   /**A Tower constructor
   *@param int xCord is the x position of the tower, also its row in the array
   *@param int yCord is the y position of the tower, also its column in the array
   *@param int money is the cost
-  *@param int rad is the radius
   */
-  public roadSpike(int xCord, int yCord, int money, int rad){
+  public RoadSpike(int xCord, int yCord, int money){
     x = xCord;
     y = yCord;
     cost = money;
-    radius = rad;
   }
 
   /**A method that takes one life from the balloon target on its tile
-  */
   public void attack(List<Balloon> balls){
     for(int i = 0; i < balls.size(); i++){
       Balloon temp = balls.get(i);
@@ -28,4 +42,5 @@ public class roadSpike extends Tower{
       }
     }
   }
+  */
 }
