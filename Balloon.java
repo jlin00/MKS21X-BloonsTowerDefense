7950b53122601e950fbadcc9ab2195340448b10c
TiffanyCao <tcao00@stuy.edu>
2019-01-19 07:43:56 -0800

    added comments

--- Balloon.java
+++ Balloon.java
@@ -21,7 +21,6 @@ public class Balloon{
   private int atTile;
 
   /**A Balloon constructor
-  *@param int each balloon has a given ID number to differentiate it easily
   *the default number of lives is 1
   */
   public Balloon(){
@@ -30,8 +29,8 @@ public class Balloon{
   }
 
   /**A second Balloon constructor
-  *@param int each balloon has a given ID nummber
   *@param int number of lives can be determined by the user
+  *the balloon is "placed" at the first road tile
   */
   public Balloon(int numLives){
     lives = numLives;
@@ -39,14 +38,21 @@ public class Balloon{
     atTile = 0;
   }
 
-  public Balloon(int numLives, int delay, int xcor, int ycor){//most specific constructor
+  /**A third and most specific Balloon constructor
+  *@param int number of lives can be determined by the user
+  *@param int delay is the interval of time in seconds that must be passed before the balloon can move to the next road tile
+  *@param int xcor is its x-coordinate on the screen
+  *@param int ycor is its y-coordinate on the screen
+  *the balloon is "placed" at the first road tile
+  */
+  public Balloon(int numLives, int delay, int xcor, int ycor){
     lives = numLives;
     isAlive = true;
     this.delay = delay;
     this.xcor = xcor;
     this.ycor = ycor;
-    atTile = 0;
-    sinceMoved = 0;
+    atTile = 0; //the balloon is "placed" at the first road tile
+    sinceMoved = 0; //this variable keeps how long it has been since the balloon last moved
   }
 
   /**A method to get the number of lives a balloon has
@@ -56,7 +62,7 @@ public class Balloon{
     return lives;
   }
 
-  /**A method to get the speed of a balloon, in terms of movement across the road
+  /**A method to get the delay of a balloon, in terms of movement across the road
   *@return int delay
   */
   public int getDelay(){
@@ -64,14 +70,14 @@ public class Balloon{
   }
 
   /**A method to check if a balloon is alive
-  *@return int xcor
+  *@return boolean isAlive
   */
   public boolean getIsAlive(){
     return isAlive;
   }
 
   /**A method to get x-coordinate of balloon
-  *@return boolean isAlive
+  *@return int xcor
   */
   public int getX(){
     return xcor;
@@ -84,13 +90,17 @@ public class Balloon{
     return ycor;
   }
 
-  /**A method to get tile of balloon
-  *@return int ycor
+  /**A method to get the road tile that the balloon is on
+  *the road tiles are numbered based on their order in the array list of road tiles
+  *@return int atTile
   */
   public int getTile(){
     return atTile;
   }
 
+  /**A method to get the time it has been since the balloon last moved
+  *it is used to check when a balloon can move again
+  */
   public long getSince(){
     return sinceMoved;
   }
@@ -115,22 +125,29 @@ public class Balloon{
     isAlive = false;
   }
 
-  /**A method to set the tile of a balloon
+  /**A method to set the road tile of a balloon
+  *@param int num (of the road tile's position in the list of road tiles)
   */
   public void setTile(int num){
     atTile = num;
   }
 
-  /**A method to move the balloon in the terminal to a new coordinate
+  /**A method to move the balloon in the terminal to the next road tile using the coordinates of that tile
+  *@param Tile t is the next road tile
+  *@param long timer is the amount of time since the last frame
   */
   public void move(Tile t, long timer){
     sinceMoved = timer;
     xcor = t.getX();
     ycor = t.getY();
     atTile++;
-    sinceMoved += delay;
+    sinceMoved += delay; //the delay is added to check that the balloon will only move after the delay time has passed
   }
 
+  /**A method that draws the balloon onto the terminal
+  **this is only used when a terminal is used for displaying the game; if a screen is used, this method is not needed
+  *@param Terminal t
+  */
   public void draw(Terminal t){
     t.moveCursor(xcor, ycor);
     t.applyBackgroundColor(Terminal.Color.WHITE);
@@ -140,6 +157,10 @@ public class Balloon{
     t.applyForegroundColor(Terminal.Color.DEFAULT);
   }
 
+  /**A method that draws the balloon onto the screen
+  *@param Screen s
+  *the color of the balloon changes depending on the amount of lives it has
+  */
   public void draw(Screen s){
     if (lives == 1) s.putString(xcor,ycor,"\u29ed",Terminal.Color.RED,Terminal.Color.WHITE);
     if (lives == 2) s.putString(xcor,ycor,"\u29ed",Terminal.Color.YELLOW,Terminal.Color.WHITE);
