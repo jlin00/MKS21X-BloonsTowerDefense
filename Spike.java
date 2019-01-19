220651c51e419be3f09fd3dac92a177b299457ca
TiffanyCao <tcao00@stuy.edu>
2019-01-19 09:27:58 -0800

    added comments

--- Spike.java
+++ Spike.java
@@ -23,6 +23,8 @@ public class Spike extends Tower{
   *@param int xCord is the x position of the tower, also its row in the array
   *@param int yCord is the y position of the tower, also its column in the array
   *@param int money is the cost
+  *@param int lives is the number of lives the spike has
+  *lives are the number of balloons a spike can pop before it "dies" and disappears
   */
   public Spike(int xCord, int yCord, int money, int lives){
     x = xCord;
@@ -31,25 +33,38 @@ public class Spike extends Tower{
     this.lives = lives;
   }
 
+  /**A method to get the number of lives the spike currently has
+  *@return int Lives
+  */
   public int getLives(){
     return lives;
   }
 
+  /**A method that takes a life away from the spike
+  */
   public void loseLife(){
     lives--;
   }
 
+  /**A method that checks if a balloon is on the same road tile as the spike
+  *a life is taken from the balloon
+  *@param List<Balloon> balloons
+  *the method reads through the list of balloons and checks their coordinates
+  */
   public void hitTarget(List<Balloon> balloons){
     for (int i = balloons.size()-1; i >= 0; i--){
       Balloon temp = balloons.get(i);
-      if (temp.getX() == x && temp.getY() == y){
+      if (temp.getX() == x && temp.getY() == y){ //if a balloon is on the same road tile
         temp.setLives(temp.getLives() - 1);
-        lives--;
-        if (temp.getLives() == 0) balloons.remove(i);
+        lives--; //the spike loses a life
+        if (temp.getLives() == 0) balloons.remove(i); //if the balloon has no more lives, it is removed
       }
     }
   }
 
+  /**A method that draws the spike onto the screen
+  *@param Screen s
+  */
   public void draw(Screen s){
     s.putString(x,y,"*",Terminal.Color.BLACK,Terminal.Color.WHITE);
   }
