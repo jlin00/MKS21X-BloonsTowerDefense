0d18f7509b14633db815c7075403f7b6bbcb22f8
Jackie Lin <jlin00@stuy.edu>
2019-01-19 22:16:29 -0500

    edited commands

--- run.sh
+++ run.sh
@@ -9,4 +9,3 @@ javac -cp lanterna.jar:. TackShooter.java
 javac -cp lanterna.jar:. Tower.java
 javac -cp lanterna.jar:. Tack.java
 javac -cp lanterna.jar:. GameScreen.java
-java -cp lanterna.jar:. GameScreen
