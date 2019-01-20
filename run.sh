c599f38d759299eb5ebdd8a4082ff34740af017b
Jackie Lin <jlin00@stuy.edu>
2019-01-19 21:23:18 -0500

    added run.sh

---
+++ run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#resize -s 105 40
+
+javac -cp lanterna.jar:. Balloon.java
+javac -cp lanterna.jar:. Tile.java
+javac -cp lanterna.jar:. SpikeTower.java
+javac -cp lanterna.jar:. Spike.java
+javac -cp lanterna.jar:. TackShooter.java
+javac -cp lanterna.jar:. Tower.java
+javac -cp lanterna.jar:. Tack.java
+javac -cp lanterna.jar:. GameScreen.java
+java -cp lanterna.jar:. GameScreen
